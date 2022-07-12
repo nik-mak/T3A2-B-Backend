bb4a8456ee3b479e6466aaa6c8237e396327d49a
limagisele <limagisele05@gmail.com>
2022-07-12 12:36:42 +1000

    added display catalogue route

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,43 @@
-const express = require("express")
-const router = express.Router()
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const authRoutes = require('./auth-routes')
-const itemRoutes = require('./item_routes')
-const userRoutes = require('./user-routes')
-const auth = require('../middleware/authenticate')
+const express = require("express");
+const router = express.Router();
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const authRoutes = require("./auth-routes");
+const itemRoutes = require("./item_routes");
+const cartRoutes = require("./cart-routes");
+const userRoutes = require("./user-routes");
+const auth = require("../middleware/authenticate");
+const ItemModel = require("../models/item");
 
 const sessionConfig = {
-  name: 'UID', // name of cookie
+  name: "UID", // name of cookie
   secret: process.env.COOKIE_SECRET, // secret that makes the cookie effective
   cookie: {
     maxAge: 1000 * 60 * 60, // time span of cookie in ms
     secure: false, // set to true in production for HTTPS only access
-    httpOnly: true // doesn't allow access from js in browser
+    httpOnly: true, // doesn't allow access from js in browser
   },
   resave: false,
   saveUninitialized: true, // set to false in production, user has to give consent
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_STORE_URI,
-    autoRemove: 'interval',
-    autoRemoveInterval: 60
-  })
-}
+    autoRemove: "interval",
+    autoRemoveInterval: 60,
+  }),
+};
 
-router.use(session(sessionConfig))
+router.use(session(sessionConfig));
 
-router.use('/auth', authRoutes)
-router.use('/items', itemRoutes)
-router.use('/user', auth, userRoutes)
+// Display full catalogue of available items on the home page
+router.get("/", (req, res) => {
+  ItemModel.find({ sold: false })
+    .then((items) => res.send(items))
+    .catch((err) => res.status(502).send({ error: err.message }));
+});
 
-module.exports = router
+router.use("/auth", authRoutes);
+router.use("/items", auth, itemRoutes);
+router.use("/user", auth, userRoutes);
+router.use("/cart", auth, cartRoutes);
+
+module.exports = router;

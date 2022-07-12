759570ab62f35a4a7ce0adcbe6383560214af67c
limagisele <limagisele05@gmail.com>
2022-07-12 12:37:40 +1000

    added separate routes for cart

---
+++ routes/cart-routes.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const ItemModel = require("../models/item");
+const UserModel = require("../models/user");
+
+// Defining that all following routes are authorised to shoppers only
+router.use((req, res, next) => {
+  req.session.user.role === "customer"
+    ? next()
+    : res.status(401).send({ error: "You don't have access!" });
+});
+
+// Add items into user's cart (order not yet created)
+router.put("/:id", async (req, res) => {
+  const item = await ItemModel.findById(req.params.id);
+  await UserModel.findByIdAndUpdate(req.session.user._id, {
+    $push: { cart: item._id },
+  });
+  res.status(201).send("Item added into cart!");
+});
+
+// Display all items inside user's cart
+router.get("/", async (req, res) => {
+  const user = await UserModel.findById(req.session.user._id).populate("cart", [
+    "_id",
+    "name",
+    "price",
+    "image",
+  ]);
+  res.status(200).send(user.cart);
+});
+
+router.delete("/:id", async (req, res) => {
+  const item = await ItemModel.findById(req.params.id);
+  await UserModel.findByIdAndUpdate(req.session.user._id, {
+    $pull: { cart: item._id },
+  });
+  res.status(200).send("Item removed from cart!");
+});
+
+// Remove all items from user's cart
+router.delete("/", async (req, res) => {
+  const user = await UserModel.findById(req.session.user._id);
+  const items = user.cart;
+  await UserModel.updateOne(user, { $pullAll: { cart: items } });
+  res.status(200).send("Your cart is now empty!");
+});
+
+module.exports = router;
--- routes/item_routes.js
+++ routes/item_routes.js
@@ -1,81 +1,80 @@
-const express = require("express")
-const router = express.Router()
-const ItemModel = require("../models/item")
+const express = require("express");
+const router = express.Router();
+const ItemModel = require("../models/item");
 
-const upload = require("../middleware/multer")
-const cloudinary = require("cloudinary").v2
+const upload = require("../middleware/multer");
+const cloudinary = require("cloudinary").v2;
 
-router.get("/", async (req, res) => {
-    await ItemModel.find()
-        .then(items => res.send(items))
-        .catch(err => res.status(502).send({ error: err.message }))
-})
-
+// Defining that all following routes are only permitted for staff and admin users
 router.use((req, res, next) => {
-    (req.session.user.role == "customer") ? res.status(401).send({ error: "You don't have access!" }) : next()
-})
+  req.session.user.role === "customer"
+    ? res.status(401).send({ error: "You don't have access!" })
+    : next();
+});
 
-router.get("/:id", async (req, res) => {
-    await ItemModel.findById(req.params.id)
-        .then(item => res.send(item))
-        .catch(err => res.status(404).send({ error: `Enable to find item with id ${req.params.id}`}))
-})
+router.get("/:id", (req, res) => {
+  ItemModel.findById(req.params.id)
+    .then((item) => res.send(item))
+    .catch((err) =>
+      res
+        .status(404)
+        .send({ error: `Enable to find item with id ${req.params.id}` })
+    );
+});
 
 router.post("/add", upload.single("image"), async (req, res) => {
+  const imageUploaded = await cloudinary.uploader.upload(req.file.path);
+
+  ItemModel.create({
+    name: req.body.name,
+    price: req.body.price,
+    size: req.body.size,
+    image: imageUploaded.secure_url,
+    imageId: imageUploaded.public_id,
+    sold: req.body.sold,
+  })
+    .then((item) => res.status(201).send(item))
+    .catch((err) => res.status(400).send({ error: err.message }));
+});
+
+router.put("/:id", upload.single("image"), async (req, res) => {
+  try {
+    const item = await ItemModel.findById(req.params.id);
     const imageUploaded = await cloudinary.uploader.upload(req.file.path);
 
-    await ItemModel.create({
+    await ItemModel.updateOne(
+      item,
+      {
         name: req.body.name,
         price: req.body.price,
         size: req.body.size,
         image: imageUploaded.secure_url,
         imageId: imageUploaded.public_id,
-        sold: req.body.sold
-    })
-        .then((item) => res.status(201).send(item))
-        .catch((err) => res.status(400).send({ error: err.message }))
-})
-
-router.put("/:id", upload.single("image"), async (req, res) => {
-    try {
-        const item = await ItemModel.findById(req.params.id)
-        const imageUploaded = await cloudinary.uploader.upload(req.file.path)          
-
-        await ItemModel.updateOne(
-          item,
-          {
-            name: req.body.name,
-            price: req.body.price,
-            size: req.body.size,
-            image: imageUploaded.secure_url,
-            imageId: imageUploaded.public_id,
-            sold: req.body.sold,
-          },
-          {
-            returnDocument: "after",
-          }
-        )
-        res.sendStatus(201)
-    }
-    catch (err) {
-        res.status(400).send({ error: err.message })
-    }    
-})
+        sold: req.body.sold,
+      },
+      {
+        returnDocument: "after",
+      }
+    );
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
 
 router.delete("/:id", async (req, res) => {
-    try {
-        const item = await ItemModel.findById(req.params.id)
+  try {
+    const item = await ItemModel.findById(req.params.id);
 
-        await cloudinary.uploader.destroy(item.imageId, function (error, result) {
-            console.log(result, error)
-        })
+    await cloudinary.uploader.destroy(item.imageId, function (error, result) {
+      console.log(result, error);
+    });
 
-        await ItemModel.deleteOne(item)
-        res.sendStatus(204)
-    }
-    catch (err) {
-        res.status(400).send({ error: err.message })
-    }    
-})
+    await ItemModel.deleteOne(item);
+    res.sendStatus(204);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
 
-module.exports = router
+module.exports = router;

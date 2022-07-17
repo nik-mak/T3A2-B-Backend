05b282d804a4a63d09a049515961c1311c2393e7
nik-mak <nick.makrakis@gmail.com>
2022-07-18 00:21:37 +1000

    changed some routes to better suit front-end needs

--- routes/cart-routes.js
+++ routes/cart-routes.js
@@ -7,26 +7,25 @@ const UserModel = require("../models/user");
 router.put("/:id", async (req, res) => {
   try {
     const item = await ItemModel.findById(req.params.id);
-    const user = await UserModel.findById(req.session.user._id)
-    
+    const user = await UserModel.findById(req.session.user._id);
+
     // Verify if item is already in the cart before adding it
     if (user.cart.includes(item._id)) {
-      throw "Item is already in the cart!"
+      throw "Item is already in the cart!";
     } else {
-      await UserModel.updateOne(user, { $push: { cart: item._id } })
+      await UserModel.updateOne(user, { $push: { cart: item._id } });
     }
-    res.status(201).send("Item added into cart!");
+    res.status(201).send(item);
   } catch (error) {
     res.status(400).send({ error });
-  }  
+  }
 });
 
 // Display all items inside user's cart
 router.get("/", async (req, res) => {
   try {
     const user = await UserModel.findById(req.session.user._id).populate(
-      "cart",
-      ["name", "price", "size", "image"]
+      "cart"
     );
     res.status(200).send(user.cart);
   } catch (err) {

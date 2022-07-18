5316d99fa7caf92e60ed92084b4befd903698caf
limagisele <limagisele05@gmail.com>
2022-07-18 21:08:39 +1000

    commented functions

--- utils/clearCart.js
+++ utils/clearCart.js
@@ -1,5 +1,6 @@
 const UserModel = require("../models/user");
 
+// removes all items from user's cart
 const clearCart = async (user, items) => {
     await UserModel.updateOne(user, { $pullAll: { cart: items } });
 }
--- utils/itemSold.js
+++ utils/itemSold.js
@@ -1,5 +1,6 @@
 const ItemModel = require("../models/item")
 
+// changes item's "sold" attribute value to true
 const itemSold = async (items) => {
     await ItemModel.updateMany({ _id: { $in: items } }, { sold: true });
 }

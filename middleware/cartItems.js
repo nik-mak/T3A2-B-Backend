94bd731b65d22f20c087e5731a417f2972cb1f42
limagisele <limagisele05@gmail.com>
2022-07-19 12:02:07 +1000

    error handling for items not available

--- middleware/cartItems.js
+++ middleware/cartItems.js
@@ -1,21 +1,33 @@
 const UserModel = require("../models/user");
+const clearCart = require("../utils/clearCart");
 
 const populatedCart = async (req, res, next) => {
-    const result = {};
-    try {
-      result.user = await UserModel.findById(req.session.user._id).populate(
-        "cart"
-      );
-      result.items = result.user.cart;
-      
-      result.totalPrice = 0;
+  const result = {};
+  try {
+    result.user = await UserModel.findById(req.session.user._id).populate(
+      "cart"
+    );
+    // verifying if there are items in the cart no longer available
+    // and removing those items from the cart
+    const itemsNotAvailable = result.user.cart.filter((el) => el.sold === true);
+    if (itemsNotAvailable.length > 0) {
+      clearCart(result.user, itemsNotAvailable);
+      throw "1 or more items in your cart is no longer available. Cart has been updated!";
+    }
+    // ensuring list of items has available items only
+    result.items = result.user.cart.filter((el) => el.sold === false);
+    // calculating order's total price
+    result.totalPrice = 0;
+    if (result.items.length > 0) {
       result.items.forEach((item) => (result.totalPrice += item.price));
       res.populatedCart = result;
-      console.log(result);
-      next();
-    } catch (err) {
-      res.status(400).send({ error: err.message });
+    } else {
+      throw "Your cart is empty!";
     }
+    next();
+  } catch (error) {
+    res.status(400).send({ error });
+  }
 };
 
 module.exports = populatedCart;

cfb6c3744bcad33dfb1d06f6785d0bc5d97126b6
limagisele <limagisele05@gmail.com>
2022-07-21 10:52:48 +1000

    set up for deployment

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-  origin : "http://localhost:3000",
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://www.desperate-housewares.com/",
+    credentials: true,
+  })
+);
 
 app.use("/api/v1", apV1Routes);
 

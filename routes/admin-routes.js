135f9af510437e69de42185c3c75e068415c1691
limagisele <limagisele05@gmail.com>
2022-07-22 08:09:52 +1000

    added missing import

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const UserModel = require("../models/user");
+const errorFormatter = require("../utils/error-format")
 
 // Get all staff and admin accounts
 router.get("/all/staff", async (req, res) => {
